19b73d299e10c46c10f857b4bd1335a79b1e53a9
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-08 23:25:57 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule
