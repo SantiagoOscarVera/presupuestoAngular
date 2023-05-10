09f1c49e5128626bd95807d581b2edd5508c751b
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-09 23:39:53 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import {Routes, RouterModule} from "@angular/router"
 import { GastosComponent } from "./components/gastos/gastos.component";
 import { IngresarPresupuestoComponent } from "./components/ingresar-presupuesto/ingresar-presupuesto.component";
 
-const routes: Routes = [
+const routes: Routes = [ // rutas
+{path: "", redirectTo:"/ingresarPresupuesto", pathMatch:"full" }, // ponemos esto para cuando este la barra sola, que vaya directo a este componente
 {path: "ingresarPresupuesto", component: IngresarPresupuestoComponent}, // aca genero la ruta hacia el componente
 {path: "gastos", component: GastosComponent},
+{path: "**", redirectTo:"/ingresarPresupuesto", pathMatch:"full"} // cuando pongo cualquier direccion me redirecciona a este componente
 ];
 
 @NgModule({
