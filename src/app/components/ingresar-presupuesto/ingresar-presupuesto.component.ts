90be60b79e13ea58211b1cc10e5b325a20992c83
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-10 00:05:37 -0300

    propiedades

--- src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ingresar-presupuesto',
   templateUrl: './ingresar-presupuesto.component.html',
   styleUrls: ['./ingresar-presupuesto.component.css']
 })
-export class IngresarPresupuestoComponent {
+export class IngresarPresupuestoComponent implements OnInit{
+  cantidad: number; //propiedad
+  cantidadIncorrecta: false;
+
+  constructor() {
+    this.cantidad = 0; // inicializamos la propiedad con estas caracteristicas
+    this.cantidadIncorrecta= false;
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
